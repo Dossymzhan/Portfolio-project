a6adfcd6bf36ffd6a68f25b20111b7617bcf8e24
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-02-20 14:03:40 +0600

    fix: fix adaptive phone nav toggle

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const menuBtn = document.querySelector(".menu__btn");
-const nav = document.querySelector('.nav__list')
+const nav = document.querySelector(".nav__list");
+const navPhone = (document.getElementById("nav-list").style.display = "flex");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
-  nav.classList.toggle('nav__open')
+  nav.classList.toggle("nav__open");
+  navPhone.style.display = "none";
 });
-
