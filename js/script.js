134b70ad64fa7e423d8f3a39dc94215c3fce43b9
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-02-20 14:50:22 +0600

    fix: overflow nav for phones

--- js/script.js
+++ js/script.js
@@ -4,9 +4,3 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   nav.classList.toggle("nav__open");
 });
-
-// const navPhone = (document.getElementById("nav-list").style.display = "none");
-
-// navPhone.addEventListener("click", () => {
-//   navPhone.style.display = "flex";
-// });
