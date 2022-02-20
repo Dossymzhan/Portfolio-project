2f9aa7cfda0135002ff3c1d6e437633f25c55724
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-02-20 13:32:33 +0600

    refactor: add phones adaptive and finish tablets

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const menuBtn = document.querySelector(".menu__btn");
+const nav = document.querySelector('.nav__list')
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+  nav.classList.toggle('nav__open')
+});
+
