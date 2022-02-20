2eb0aeb8ada10ced314b359e33157b104d41fe9d
Dossymzhan Balagazhinov <dosymjan6870@gmail.com>
2022-02-20 14:25:24 +0600

    fix: overflow fix

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
 const menuBtn = document.querySelector(".menu__btn");
 const nav = document.querySelector(".nav__list");
-const navPhone = (document.getElementById("nav-list").style.display = "flex");
 menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("open");
   nav.classList.toggle("nav__open");
-  navPhone.style.display = "none";
 });
+
+// const navPhone = (document.getElementById("nav-list").style.display = "none");
+
+// navPhone.addEventListener("click", () => {
+//   navPhone.style.display = "flex";
+// });
